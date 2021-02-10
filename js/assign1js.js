114467373e30277d8d64b3b807716210fa1250e9
Darren <darren.lediard@gmail.com>
2021-02-09 23:38:24 -0500

    Added Buttons

--- js/assign1js.js
+++ js/assign1js.js
@@ -16,18 +16,33 @@ function printResult()
 
     if (average >= 90)
     {
-        document.getElementById("result").innerHTML= userName + "<br>Your average is:" + average + " Awesome";
+        document.getElementById("result").innerHTML= userName + "<br>Your average is: " + average + " Awesome";
     }
     else if ((average < 90) && (average >= 80))
     {
-        document.getElementById("result").innerHTML= userName + "<br>Your average is:" + average + " Very Good";
+        document.getElementById("result").innerHTML= userName + "<br>Your average is: " + average + " Very Good";
     }
     else if ((average < 80) && (average >= 50))
     {
-        document.getElementById("result").innerHTML= userName + "<br>Your average is:" + average + " OK";
+        document.getElementById("result").innerHTML= userName + "<br>Your average is: " + average + " OK";
     }
-    else 
+    else
     {
-         document.getElementById("result").innerHTML= userName + "<br>Your average is:" + average + " Fail";
-    }  
+         document.getElementById("result").innerHTML= userName + "<br>Your average is: " + average + " Fail";
+    }
+}
+
+function printChristmas()
+{
+  document.getElementById("holiday").innerHTML = "This is Christmas";
+}
+
+function printHalloween()
+{
+  document.getElementById("holiday").innerHTML = "This is Halloween";
+}
+
+function printCNewYears()
+{
+  document.getElementById("holiday").innerHTML = "This is New Years";
 }
